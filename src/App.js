6de56b5aaa3293cc2ea3f1c174473ac7d49f7b8e
lppduy <lephamphuongduy@gmail.com>
2023-09-08 20:52:40 +0700

    default page setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Products from './components/Products';
+import About from './components/About';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link
+            to="/"
+            style={{ textDecoration: 'none' }}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/products"
+            style={{ textDecoration: 'none' }}
+          >
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/about"
+            style={{ textDecoration: 'none' }}
+          >
+            About
+          </Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/products"
+          element={<Products />}
+        />
+        <Route
+          path="/about"
+          element={<About />}
+        />
+      </Routes>
     </div>
   );
 }
